3cd92202853fee0d4fe4a0d4bb7e64c1117e66db
naveen741 <96760074+naveen741@users.noreply.github.com>
2022-04-12 12:19:13 +0530

    Update Stack.java

--- src/main/java/datastructure/Stack.java
+++ src/main/java/datastructure/Stack.java
@@ -1,8 +1,11 @@
 package datastructure;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class Stack {
 	ArrayList<Integer> list;
 	int size; 
+	static Logger logger=Logger.getLogger(Stack.class.getName());
 	/**
 	 * Stack is constructor to initialize the @size @list
 	 * @param max is maximum size of the Stack
@@ -19,7 +22,7 @@ public class Stack {
 		if(list.size()<this.size)
 			list.add(data);
 		else
-			System.out.println("Can't push Stack is overFlow");
+			logger.log(Level.INFO,"Cant push Stack is overFlow");
 	}
 	/**
 	 * pop is method to delete last element in Stack
@@ -28,57 +31,60 @@ public class Stack {
 		if(!list.isEmpty())
 			list.remove(list.size()-1);
 		else
-			System.out.println("Can't pop Stack is underFlow.");
+			logger.log(Level.INFO,"Can't pop Stack is underFlow.");
 	}
 	/**
 	 * peek is method returns the top value of Stack 
 	 */
 	void peek() {
-		System.out.println("top (peek) element : "+list.get(list.size()-1));
+		logger.log(Level.INFO,"top (peek) element : {0}",list.get(list.size()-1));
 	}
 	/**
 	 * display method prints the stack element
 	 */
 	void display() {
+		StringBuilder str=new StringBuilder();
 		for(int i=0;i<list.size();i++) {
-			System.out.print(list.get(i)+" ");
+			if(i==list.size()-1)
+				str.append(list.get(i));
+			else
+				str.append(list.get(i)+"-");
 		}
-		System.out.println();
+		String temp=str.toString();
+		logger.log(Level.INFO,"{0}",temp);
 	}
 	/**
 	 * isEmpty checks the stack is empty or not
 	 */
 	void isEmpty() {
-		if(list.isEmpty()) {
-			System.out.println("Stack is emptied.");
-		}
-		else {
-			System.out.println("Stack is not emptied.");
-		}
+		if(list.isEmpty()) 
+			logger.log(Level.INFO,"Stack is emptied.");
+		else 
+			logger.log(Level.INFO,"Stack is not emptied.");
 	}
 	public static void main(String[] args) {
 		Stack s1=new Stack(15);
-		System.out.println("Push operation");
+		logger.log(Level.INFO,"Push operation");
 		s1.push(7);
 		s1.push(4);
 		s1.push(7);
 		s1.push(1);
 		s1.push(6);
 		s1.display();
-		System.out.println("Checking stack is empty or not...");
+		logger.log(Level.INFO,"Checking stack is empty or not...");
 		s1.isEmpty();
-		System.out.println("peek element operation");
+		logger.log(Level.INFO,"peek element operation");
 		s1.peek();
-		System.out.println("Pop operation");
+		logger.log(Level.INFO,"Pop operation");
 		s1.pop();
 		s1.pop();
 		s1.pop();
 		s1.display();
-		System.out.println("peek element operation");
+		logger.log(Level.INFO,"peek element operation");
 		s1.peek();
 		s1.pop();
 		s1.pop();
-		System.out.println("Checking stack is empty or not...");
+		logger.log(Level.INFO,"Checking stack is empty or not...");
 		s1.isEmpty();
 		s1.pop();
 	}
